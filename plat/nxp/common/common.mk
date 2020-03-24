ffb70bba76ebaa64bec69d49a19da6e742fa0907
Rabeeh Khoury <rabeeh@solid-run.com>
2020-03-24 02:48:34 +0200

    plat/nxp: lx2160a auto boot
    This patch adds support to patch RCW that already has SD/eMMC/SPI boot
    support embedded with conditional load and jump.
    The idea is to look for SD/eMMC/SPI boot, and modify src/dst/size
    address with the correct values; rather than adding blockread at the end
    of RCW code.
    
    With this patch images are unified and can be used to boot from SD /
    eMMC and SPI.
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- plat/nxp/common/common.mk
+++ plat/nxp/common/common.mk
@@ -148,6 +148,11 @@ BOOT_DEV_SOURCES		=	${PLAT_DRIVERS_PATH}/sd/sd_mmc.c	\
 else ifeq (${BOOT_MODE}, flexspi_nor)
 $(eval $(call add_define,FLEXSPI_NOR_BOOT))
 BOOT_DEV_SOURCES		= ${PLAT_DRIVERS_PATH}/flexspi/nor/flexspi_nor.c
+else ifeq (${BOOT_MODE}, auto)
+$(eval $(call add_define,FLEXSPI_NOR_BOOT))
+BOOT_DEV_SOURCES		= ${PLAT_DRIVERS_PATH}/flexspi/nor/flexspi_nor.c \
+				  ${PLAT_DRIVERS_PATH}/sd/sd_mmc.c	\
+				  drivers/io/io_block.c
 endif
 
 # DDR driver needs to be enabled by default
